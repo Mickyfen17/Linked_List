a1349cead34dcf418aaafef3b7ac83844470396d
Michael Fenwick <mickyfen17@aol.com>
2016-12-11 17:08:45 -0700

    Break out large createStuff function into small helper functions

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var inputTitle = document.getElementById("website-title");
 var inputURL = document.getElementById("website-url");
 var enterButton = document.getElementById("enter-button");
+var bookmarkSection = document.querySelector(".bookmark-section");
+var newDiv;
+var newH3;
+var newH4;
+var newATag;
+var newBtnDiv;
+var newReadBtn;
+var newDeleteBtn;
 var userBookmarkData = [];
 
 function grabUserData() {
@@ -11,67 +19,61 @@ function grabUserData() {
 
 enterButton.addEventListener("click", function() {
   grabUserData();
-  createStuff();
+  createBookmarkDiv();
   console.log(userBookmarkData[0], userBookmarkData[1]);
 });
 
-var bookmarkSection = document.querySelector(".bookmark-section");
-var newDiv;
-var newH3;
-var newH4;
-var newATag;
-var newBtnDiv;
-var newReadBtn;
-var newDeleteBtn;
-
-function createStuff() {
+function createBookmarkDiv() {
   // create div with classname bookmarks
   newDiv = document.createElement("div");
   newDiv.className += "bookmarks";
-
-  // newDiv.appendChild(newH3);
-
-  // create h3 with class bookmark-title
+  createBookmarkTitle();
+}
+function createBookmarkTitle() {
   newH3 = document.createElement("h3");
   newH3.className += "bookmark-title";
   var inputTitleTextNode = document.createTextNode(userBookmarkData[0]);
   newH3.appendChild(inputTitleTextNode);
+  createBookmarkURL();
+}
 
-  // create h4 with class bookmark-url
+function createBookmarkURL() {
   newH4 = document.createElement("h4");
   newH4.className += "bookmark-url";
-  // create a tag for h4
+  createHREF();
+}
+function createHREF() {
   newATag = document.createElement("a");
   var inputURLTextNOde = document.createTextNode(userBookmarkData[1]);
-  newATag.href = userBookmarkData[1];
+  newATag.href = "http://" + userBookmarkData[1];
   newATag.appendChild(inputURLTextNOde);
   newH4.appendChild(newATag);
-
-  // create div for buttons with class read-delete-btns-cont
+  createButtonDiv();
+}
+function createButtonDiv() {
   newBtnDiv = document.createElement("div");
   newBtnDiv.className += "read-delete-btns-cont";
-
-  // newBtnDiv.appendChild(newReadBtn);
-  // newBtnDiv.appendChild(newDeleteBtn);
-
-  // create div for buttons with class read-button
+  createReadButton();
+}
+function createReadButton() {
   newReadBtn = document.createElement("button");
   newReadBtn.className += "read-button";
   var readButtonText = document.createTextNode("Read");
   newReadBtn.appendChild(readButtonText);
-
-  // create div for buttons with class delete-button
+  createDeleteButton();
+}
+function createDeleteButton() {
   newDeleteBtn = document.createElement("button");
   newDeleteBtn.className += "delete-button";
   var deleteButtonText = document.createTextNode("Delete");
   newDeleteBtn.appendChild(deleteButtonText);
-
+  appendData();
+}
+function appendData() {
   newDiv.appendChild(newH3);
   newDiv.appendChild(newH4);
   newBtnDiv.appendChild(newReadBtn);
   newBtnDiv.appendChild(newDeleteBtn);
   newDiv.appendChild(newBtnDiv);
-
   bookmarkSection.appendChild(newDiv);
-
 }
