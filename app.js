f3c0cdf79258c400e3475937393821bcf090c233
Michael Fenwick <mickyfen17@aol.com>
2016-12-13 16:31:55 -0700

    Remove className += and replaced with classList.add

--- app.js
+++ app.js
@@ -37,11 +37,9 @@ function testForEmptyInputs() {
 // Throw alert if one box has content and the other is empty upon Enter click
 function alertError() {
   if(inputTitle.value.length > 2 && inputURL.value.length === 0) {
-    // alert("Please enter a valid URL");
     errorText.innerText = "Please Enter A Valid URL";
     throw new Error("Please enter a valid URL");
   } else if (inputURL.value.length > 2 && inputTitle.value.length === 0) {
-    // alert("Please enter a valid Website Title");
     errorText.innerText = "Please Enter A Valid Website Title";
     throw new Error("Please enter a valid Website Title");
   }
@@ -59,19 +57,18 @@ enterButton.addEventListener("click", function() {
   grabUserData();
   createBookmarkDiv();
   clearInputs();
-  console.log(userBookmarkData[0], userBookmarkData[1]);
 });
 
 // Create main outer div bookmark
 function createBookmarkDiv() {
   newDiv = document.createElement("div");
-  newDiv.className += "bookmarks";
+  newDiv.classList.add("bookmarks");
   createBookmarkTitle();
 }
 // Create bookmark title and append user input data
 function createBookmarkTitle() {
   newH3 = document.createElement("h3");
-  newH3.className += "bookmark-title";
+  newH3.classList.add("bookmark-title");
   var inputTitleTextNode = document.createTextNode(userBookmarkData[0]);
   newH3.appendChild(inputTitleTextNode);
   createBookmarkURL();
@@ -79,7 +76,7 @@ function createBookmarkTitle() {
 // Create h4 to store a tag and website link
 function createBookmarkURL() {
   newH4 = document.createElement("h4");
-  newH4.className += "bookmark-url";
+  newH4.classList.add("bookmark-url");
   createHREF();
 }
 // Create a tag and website link with user data and append to h4
@@ -94,32 +91,35 @@ function createHREF() {
 // Create div to house Read and Delete buttons
 function createButtonDiv() {
   newBtnDiv = document.createElement("div");
-  newBtnDiv.className += "read-delete-btns-cont";
+  newBtnDiv.classList.add("read-delete-btns-cont");
   createReadButton();
 }
 // Create Read button and add Eventlistener
 function createReadButton() {
   newReadBtn = document.createElement("button");
-  newReadBtn.className += "read-button";
+  newReadBtn.classList.add("read-button");
   var readButtonText = document.createTextNode("Read");
   newReadBtn.appendChild(readButtonText);
   newReadBtn.addEventListener("click", function() {
-    readBtnClassToggle(this);
+    bookmarkClassToggle(this);
   });
   createDeleteButton();
 }
 // Function to add or remove read class
-function readBtnClassToggle(btn) {
+function bookmarkClassToggle(btn) {
+  var bookmarkCard = btn.parentNode.parentNode;
   if(btn.classList.contains("read")) {
     btn.classList.remove("read");
+    bookmarkCard.classList.remove("bookmarkRead");
   } else {
     btn.classList.add("read");
+    bookmarkCard.classList.add("bookmarkRead");
   }
 }
 // Create Delete button and add eventlistener
 function createDeleteButton() {
   newDeleteBtn = document.createElement("button");
-  newDeleteBtn.className += "delete-button";
+  newDeleteBtn.classList.add("delete-button");
   var deleteButtonText = document.createTextNode("Delete");
   newDeleteBtn.appendChild(deleteButtonText);
   newDeleteBtn.addEventListener("click", function() {
